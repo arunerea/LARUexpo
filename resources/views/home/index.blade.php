fd34037d1e776dc92abe22dd2aced507dccfde4a
The Great Aru <mirenaizaguirre@buenosaires.gob.ar>
2025-09-19 14:51:26 -0300

    Refactor : pink color scheme for buttons and titles

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,8 +4,8 @@
         <div class="flex justify-center mt-12">
             <form class="w-full max-w-xl flex items-center bg-white rounded-full shadow-md px-4 py-2">
                 <label for="search" class="sr-only">Buscar</label>
-                <input type="text" id="search" name="search" placeholder="Buscar..." class="flex-grow px-4 py-3 rounded-l-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"/>
-                <button type="submit" class="bg-blue-600 text-white rounded-r-full px-6 py-3 hover:bg-blue-700 transition font-semibold">
+                <input type="text" id="search" name="search" placeholder="Buscar..." class="flex-grow px-4 py-3 rounded-l-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent"/>
+                <button type="submit" class="bg-pink-400 text-white rounded-r-full px-6 py-3 hover:bg-pink-700 transition font-semibold">
                 Buscar
                 </button>
             </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title>LARU.</title>
         @vite('resources/css/app.css')
     </head>
@@ -12,20 +13,20 @@
         <div class="flex justify-between items-center px-6 py-4">
             <div class="flex space-x-4 items-center">
                 <h1 class="titulo">LARU.</h1>
-                <a href="#" class="flex items-center text-blue-600 hover:underline">
+                <a href="#" class="flex items-center text-pink-400 hover:underline">
                     <i class="fa-solid fa-location-dot ml-10 mr-3 text-xl"></i> Ubicación
                 </a>
             </div>
             <div class="flex space-x-4 items-center">
-                <a href="/login" class="bg-blue-500 text-white px-5 py-2 rounded hover:bg-blue-200 transition">
+                <a href="/login" class="bg-pink-400 text-white px-5 py-2 rounded hover:bg-pink-200 transition">
                     Login
                 </a>
-                <a href="/register" class="bg-blue-800 text-white px-5 py-2 rounded hover:bg-blue-200 transition">
+                <a href="/register" class="bg-pink-600 text-white px-5 py-2 rounded hover:bg-pink-200 transition">
                     Register
                 </a>
             </div>
         </div>
-        <hr>
+        <hr class="border-gray-300">
         @yield('content')
         @vite('resources/js/app.js')
     </body>
